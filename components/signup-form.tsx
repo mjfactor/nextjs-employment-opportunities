168f55bc2a2c1331bf296884967c45f612092234
mjfactor <emjayfactor@gmail.com>
2025-03-03 17:29:26 +0800

    just commit

--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -69,9 +69,6 @@ export function SignupForm({ className, ...props }: React.ComponentPropsWithoutR
           </div>
         </div>
       </form>
-      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
-      </div>
     </div>
   )
 }
