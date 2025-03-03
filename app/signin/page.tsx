2932e00f8492fcc228db909b2c94999eb6648eff
mjfactor <emjayfactor@gmail.com>
2025-03-04 00:43:29 +0800

    addded sign in, sign out, middleware

---
+++ app/signin/page.tsx
@@ -0,0 +1,16 @@
+import { LoginForm } from "@/components/login-form"
+import { auth } from "@/auth"
+import { redirect } from "next/navigation"
+export default async function LoginPage() {
+    const session = await auth()
+    if (session) {
+        redirect("/dashboard")
+    }
+    return (
+        <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+            <div className="w-full max-w-sm">
+                <LoginForm />
+            </div>
+        </div>
+    )
+}
