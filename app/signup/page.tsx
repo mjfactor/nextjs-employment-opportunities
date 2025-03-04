f55f38741535be9f64c4a10912f524feecbc94e2
mjfactor <emjayfactor@gmail.com>
2025-03-04 10:08:40 +0800

    something is happening

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,6 +1,11 @@
 import { SignupForm } from "../../components/signup-form"
-
-export default function SignupPage() {
+import { auth } from "@/auth"
+import { redirect } from "next/navigation"
+export default async function SignupPage() {
+  const session = await auth()
+    if (session) {
+        redirect("/dashboard")
+    }
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
       <div className="w-full max-w-sm">
