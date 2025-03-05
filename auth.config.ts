b9239b7ce4313a7916833b6124ca5d90647b199d
mjfactor <emjayfactor@gmail.com>
2025-03-05 17:02:51 +0800

    added zod

--- auth.config.ts
+++ auth.config.ts
@@ -1,8 +1,12 @@
 import type { NextAuthConfig } from "next-auth"
 import Google from "next-auth/providers/google"
+import Credentials from "next-auth/providers/credentials"
 // Notice this is only an object, not a full Auth.js instance
 export default {
     providers: [
-        Google
+        Google,
+        Credentials({
+            
+        })
     ],
 } satisfies NextAuthConfig
